00c35efc0723d568a9bfa7f9dbb4a926415997f2
SolarAweTM <97848738+SolarAweTM@users.noreply.github.com>
2025-10-30 13:07:14 -0500

    Remove sophisticated drawer recipes

---
+++ kubejs/server_scripts/sophisticated_storage.js
@@ -0,0 +1,36 @@
+// priority: 0
+
+console.info('-KubeJS- Sophisticated Storage server script loaded.')
+
+ServerEvents.recipes(event => {
+	let soph_drawers = [
+		'sophisticatedstorage:limited_barrel_1',
+		'sophisticatedstorage:limited_barrel_2',
+		'sophisticatedstorage:limited_barrel_3',
+		'sophisticatedstorage:limited_barrel_4',
+		'sophisticatedstorage:limited_iron_barrel_1',
+		'sophisticatedstorage:limited_iron_barrel_2',
+		'sophisticatedstorage:limited_iron_barrel_3',
+		'sophisticatedstorage:limited_iron_barrel_4',
+		'sophisticatedstorage:limited_copper_barrel_1',
+		'sophisticatedstorage:limited_copper_barrel_2',
+		'sophisticatedstorage:limited_copper_barrel_3',
+		'sophisticatedstorage:limited_copper_barrel_4',
+		'sophisticatedstorage:limited_gold_barrel_1',
+		'sophisticatedstorage:limited_gold_barrel_2',
+		'sophisticatedstorage:limited_gold_barrel_3',
+		'sophisticatedstorage:limited_gold_barrel_4',
+		'sophisticatedstorage:limited_diamond_barrel_1',
+		'sophisticatedstorage:limited_diamond_barrel_2',
+		'sophisticatedstorage:limited_diamond_barrel_3',
+		'sophisticatedstorage:limited_diamond_barrel_4',
+		'sophisticatedstorage:limited_netherite_barrel_1',
+		'sophisticatedstorage:limited_netherite_barrel_2',
+		'sophisticatedstorage:limited_netherite_barrel_3',
+		'sophisticatedstorage:limited_netherite_barrel_4'
+	]
+	//remove all sophisticated drawer (limited barrel) recipes
+	soph_drawers.forEach(item => {
+		event.remove({output: item})
+	})
+})
