cb45abb9f056b77429d878ea38f3321317d0cac3
Reef <97848738+SolarAweTM@users.noreply.github.com>
2026-07-13 22:55:56 -0500

    Add catalyst and artifact sprites

--- kubejs/startup_scripts/startup_main_script.js
+++ kubejs/startup_scripts/startup_main_script.js
@@ -31,12 +31,12 @@ StartupEvents.registry('item', event => {
 	//flower shop token
 	event.create('flower_token').texture('kubejs:item/example_item') //TODO: Give proper texture
 	//tier 4 and 5 grader catalysts
-	event.create('catalyst_4').texture('kubejs:item/example_item') //TODO: Give proper texture
+	event.create('catalyst_4').texture('srsp:item/catalyst_4') // Fehdison
 	event.create('catalyst_5').texture('kubejs:item/example_item') //TODO: Give proper texture
 	//rainbow furnace components
 	event.create('newest_artifact').texture('srsp:item/shadow_crystal')
 	event.create('yaoi_artifact').texture('srsp:item/brutal_brass') // [03]
-	event.create('yuri_artifact').texture('kubejs:item/example_item') //TODO: Give proper texture
+	event.create('yuri_artifact').texture('srsp:item/yuri_manga') // Fehdison
 	event.create('sugoi_artifact').texture('kubejs:item/example_item') //TODO: Give proper texture
 	
 })
